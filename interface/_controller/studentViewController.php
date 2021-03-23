d9c0abf166081f2e69e91a75d856d0b3f6ab2d4b
Romain Lefranc <rlefranc@lefresnoy.net>
2021-03-23 18:36:43 +0100

    changing redirection method

--- interface/_controller/studentViewController.php
+++ interface/_controller/studentViewController.php
@@ -38,13 +38,10 @@
     //display the scripts
     scripts('countdown');
 
-
-
     //check if there is any information on the url
     if(isset($_GET) && !empty($_GET)){
 
-
-        if( isset($_GET['action']) && !empty($_GET['action']) && isset($_POST)   &&  !empty($_POST)){
+        if( isset($_GET['action']) && !empty($_GET['action']) && isset($_POST)  && !empty($_POST)){
                 //Convert all applicable characters to HTML entities
                 $title          = htmlentities($_POST['title']);
                 $subtitle       = htmlentities($_POST['subtitle']);
@@ -53,91 +50,91 @@
                 $short_syn      = htmlentities($_POST['synopsis_short']);
                 $long_syn       = htmlentities($_POST['synopsis_long']);
                 $thanks         = htmlentities($_POST['thanks']);
-                $img            = checkFiles($_FILES);
+                // $img            = checkFiles($_FILES);
 
                 //if it's a creation
-            if($_GET['action']=="create"){
+                if($_GET['action']=="create"){
 
-                //set automatically the current date and seen = false
-                date_default_timezone_set("Europe/Paris");
-                $created_date   = date("Y-m-d");
-                $seen = 0;
+                    //set automatically the current date and seen = false
+                    date_default_timezone_set("Europe/Paris");
+                    $created_date   = date("Y-m-d");
+                    $seen = 0;
 
-                //catch idStudent
-                $id_student     = $_SESSION['idStudent'];
+                    //catch idStudent
+                    $id_student     = $_SESSION['idStudent'];
 
-                //create an object Artwork
-                $aw = new Artwork();
-                $aw ->setTitle($title)->setSubtitle($subtitle)->setType($type)->setDuration($duration)
-                    ->setSynopsisShort($short_syn)->setSynopsisLong($long_syn)
-                    ->setThanks($thanks)
-                    ->setCreatedAt($created_date)->setIdStudent($id_student)->setSeen($seen);
+                    //create an object Artwork
+                    $aw = new Artwork();
+                    $aw ->setTitle($title)->setSubtitle($subtitle)->setType($type)->setDuration($duration)
+                        ->setSynopsisShort($short_syn)->setSynopsisLong($long_syn)
+                        ->setThanks($thanks)
+                        ->setCreatedAt($created_date)->setIdStudent($id_student)->setSeen($seen);
 
-                try{
-                    //send the request throw several layer.
-                    //can catch a success if operation happened well - to display a success alert 1=success 0=fail
-                   $success=ArtworkService::create($aw);
-                   if($success){
-                       //send to the pre load artwork form
-                      header('location: ../_controller/studentViewController');
-                   }
+                    try{
+                        //send the request throw several layer.
+                        //can catch a success if operation happened well - to display a success alert 1=success 0=fail
+                       $success=ArtworkService::create($aw);
+                       if($success){
+                           //send to the pre load artwork form
+                          // header('location: ../_controller/studentViewController.php');
+                          echo "<meta http-equiv='refresh' content='0;url=../_controller/studentViewController.php'>";
+                       }
 
-
-                }catch(ServiceException $serviceException){
-                    echo $ServiceException->getCode();
-                }
-            }
-
-
-            //if it's an update
-            elseif($_GET['action']=="edit"){
-                //keep only the comparable values (not id, created_at, id_student and seen)
-                $old_content_array = array_slice((array) $session_artwork_obj, 1, 7);
-
-                $new_content_array = [$title, $subtitle, $type, $duration, $short_syn, $long_syn, $thanks];
-
-                try{
-                    //create updates obj
-                    $i=0;
-                    foreach($old_content_array as $value){
-
-                        //compare index by index
-                        if($value != $new_content_array[$i]){
-
-                            $input_name = preg_replace('/artwork/i', "", array_search($value, $old_content_array));
-
-                            $old_content    =   $value;
-                            $new_content    = $new_content_array[$i];
-
-                            $updated_at     = date("Y-m-d");
-                            $id_artwork     = $session_artwork_obj->getId();
-
-                            $update = new Update();
-                            $update ->setUpdatedDate($updated_at)->setInputName($input_name)->setOldContent($old_content)
-                                    ->setNewContent($new_content)->setIdArtwork($id_artwork)->setIsSeen(0);
-
-                            $update_is_ok = UpdateService::create($update);
-                        }
-                        $i++;
+                    }catch(ServiceException $serviceException){
+                        echo $ServiceException->getCode();
                     }
-
-                    //update the current artwork
-                    $updated_artwork = new Artwork();
-                    $updated_artwork    ->setTitle($title)->setSubtitle($subtitle)->setType($type)->setDuration($duration)
-                                        ->setSynopsisShort($short_syn)->setSynopsisLong($long_syn)
-                                        ->setThanks($thanks)->setSeen(0);
-
-                    $success=ArtworkService::update($updated_artwork, $session_artwork_obj->getId());
-
-
-                    //send to the pre load artwork form
-
-
-
-                }catch(ServiceException $serviceException){
-                    echo $ServiceException->getCode();
                 }
-            }
+
+
+                // if it's an update
+                elseif($_GET['action']=="edit"){
+                    //keep only the comparable values (not id, created_at, id_student and seen)
+                    $old_content_array = array_slice((array) $session_artwork_obj, 1, 7);
+
+                    $new_content_array = [$title, $subtitle, $type, $duration, $short_syn, $long_syn, $thanks];
+
+                    try{
+                        //create updates obj
+                        $i=0;
+                        foreach($old_content_array as $value){
+
+                            //compare index by index
+                            if($value != $new_content_array[$i]){
+
+                                $input_name = preg_replace('/artwork/i', "", array_search($value, $old_content_array));
+
+                                $old_content    =   $value;
+                                $new_content    = $new_content_array[$i];
+
+                                $updated_at     = date("Y-m-d");
+                                $id_artwork     = $session_artwork_obj->getId();
+
+                                $update = new Update();
+                                $update ->setUpdatedDate($updated_at)->setInputName($input_name)->setOldContent($old_content)
+                                        ->setNewContent($new_content)->setIdArtwork($id_artwork)->setIsSeen(0);
+
+                                $update_is_ok = UpdateService::create($update);
+                            }
+                            $i++;
+                        }
+
+                        //update the current artwork
+                        $updated_artwork = new Artwork();
+                        $updated_artwork    ->setTitle($title)->setSubtitle($subtitle)->setType($type)->setDuration($duration)
+                                            ->setSynopsisShort($short_syn)->setSynopsisLong($long_syn)
+                                            ->setThanks($thanks)->setSeen(0);
+
+                        $success=ArtworkService::update($updated_artwork, $session_artwork_obj->getId());
+                        echo "<meta http-equiv='refresh' content='0;url=../_controller/studentViewController.php'>";
+
+                        //send to the pre load artwork form
+
+
+
+                    }catch(ServiceException $serviceException){
+                        echo $ServiceException->getCode();
+                    }
+                }
         }
     }
 ?>
