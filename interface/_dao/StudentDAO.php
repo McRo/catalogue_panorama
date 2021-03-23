c62fca77fd4f608d1901178c38fd6ced9ee5f51a
Romain Lefranc <rlefranc@lefresnoy.net>
2021-03-23 17:33:53 +0100

    fix merge

--- interface/_dao/StudentDAO.php
+++ interface/_dao/StudentDAO.php
@@ -1,13 +1,12 @@
 <?php
     require_once(__DIR__.'/../exception/DAOException.php');
     require_once(__DIR__.'/Connection.php');
-    require_once(__DIR__.'/../interface/InterfaceDao.php');
 
     class StudentDAO extends Connection {
 
         //add new artwork
         public function create(Object $student){
-             
+
              $id_user = $student->getIdUser();
             $nickname = $student->getNickname();
             $bio_short_fr = $student->getBioShortFr();
@@ -20,18 +19,18 @@
 
             try{
                 //connect to the bdd
-                $db= Connection::connect();                 
+                $db= Connection::connect();
                 //insert request
                 $stmt = $db->prepare("INSERT INTO `student` VALUES (NULL, :id_user, :nickname, :bio_short_fr, :bio_fr, :bio_short_en, :bio_en, :facebook, :twitter, :website)");
                 //binding params
-                $stmt->bindParam(':id_user', $id_user); 
-                $stmt->bindParam(':nickname', $nickname); 
-                $stmt->bindParam(':bio_short_fr', $bio_short_fr); 
-                $stmt->bindParam(':bio_fr', $bio_fr); 
-                $stmt->bindParam(':bio_short_en', $bio_short_en); 
-                $stmt->bindParam(':bio_en', $bio_en); 
-                $stmt->bindParam(':facebook', $facebook); 
-                $stmt->bindParam(':twitter', $twitter); 
+                $stmt->bindParam(':id_user', $id_user);
+                $stmt->bindParam(':nickname', $nickname);
+                $stmt->bindParam(':bio_short_fr', $bio_short_fr);
+                $stmt->bindParam(':bio_fr', $bio_fr);
+                $stmt->bindParam(':bio_short_en', $bio_short_en);
+                $stmt->bindParam(':bio_en', $bio_en);
+                $stmt->bindParam(':facebook', $facebook);
+                $stmt->bindParam(':twitter', $twitter);
                 $stmt->bindParam(':website', $website);
 
                 $rs = $stmt->execute();
@@ -62,21 +61,21 @@
             try{
                 echo$idStudent;
                 //connect to the bdd
-                $db= Connection::connect(); 
-                $collected_info =   'student.id, student.id_user, student.nickname, student.bio_short_fr, student.bio_fr, 
-                                    student.facebook, student.twitter, student.website, user.first_name, user.last_name, 
-                                    user.email, artwork.id"id_artwork", artwork.title, artwork.subtitle, artwork.type, artwork.duration, 
+                $db= Connection::connect();
+                $collected_info =   'student.id, student.id_user, student.nickname, student.bio_short_fr, student.bio_fr,
+                                    student.facebook, student.twitter, student.website, user.first_name, user.last_name,
+                                    user.email, artwork.id"id_artwork", artwork.title, artwork.subtitle, artwork.type, artwork.duration,
                                     artwork.synopsis_short, artwork.synopsis_long, artwork.thanks, artwork.seen';
 
-                $stmt=$db->prepare("SELECT $collected_info FROM (student INNER JOIN user ON student.id_user = user.id) 
-                INNER JOIN artwork on student.id= artwork.id_student 
+                $stmt=$db->prepare("SELECT $collected_info FROM (student INNER JOIN user ON student.id_user = user.id)
+                INNER JOIN artwork on student.id= artwork.id_student
                 WHERE student.id=$idStudent");
                 $stmt->execute();
-                //store the result into data, returns an array indexed by column name 
+                //store the result into data, returns an array indexed by column name
                 $data = $stmt->fetch(PDO::FETCH_ASSOC);
                 //free the memory
                 $stmt->closeCursor();
-                
+
                 return $data;
 
             }catch(PDOException $e){
@@ -89,13 +88,13 @@
         public function searchByUser(Int $idUser){
             try{
                 //connect to the bdd
-                $db= Connection::connect(); 
+                $db= Connection::connect();
 
                 $stmt=$db->prepare("SELECT * FROM student WHERE id_user=$idUser");
                 $stmt->execute();
-                //store the result into data, returns an array indexed by column name 
+                //store the result into data, returns an array indexed by column name
                 $data = $stmt->fetch(PDO::FETCH_ASSOC);
-                
+
                 //free the memory
                 $stmt->closeCursor();
                 print_r($data);
@@ -110,14 +109,14 @@
         public function searchByArtwork(int $idArtwork){
             try{
                 //connect to the bdd
-                $db= Connection::connect(); 
+                $db= Connection::connect();
 
                 $stmt=$db->prepare("SELECT id_student FROM artwork WHERE id=:id_artwork");
                 $stmt->bindParam(':id_artwork', $idArtwork);
                 $stmt->execute();
-                //store the result into data, returns an array indexed by column name 
+                //store the result into data, returns an array indexed by column name
                 $data = $stmt->fetch(PDO::FETCH_ASSOC);
-                
+
                 //free the memory
                 $stmt->closeCursor();
                 return $data;
@@ -131,8 +130,8 @@
 
         // }
 
-        
 
-        
+
+
     }
 ?>
